09d45011da569f24cbc1c18042033eedcdf2698b
Vinícius Bastos Gomes <42478992+ViniciusBG@users.noreply.github.com>
2020-09-05 14:10:32 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import plotly.graph_objects as go
 LTN = "https://apiapex.tesouro.gov.br/aria/v1/sistd/custom/historico?ano=2020&idSigla=2"
 LFT = "https://apiapex.tesouro.gov.br/aria/v1/sistd/custom/historico?ano=2020&idSigla=1"
 NTNB_p= "https://apiapex.tesouro.gov.br/aria/v1/sistd/custom/historico?ano=2020&idSigla=5"
-
+IPCA = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.16121/dados?formato=json'
 
 def dolar():
     y = requests.get('https://api.bcb.gov.br/dados/serie/bcdata.sgs.10813/dados?formato=json').content
@@ -20,23 +20,37 @@ def dolar():
     dolar = dolar[dolar['data']>'01/01/2020']
     dolar_plot = px.line(data_frame=dolar,x='data',y='valor')
     return dolar_plot
+def ipca():
+    y = requests.get(IPCA).content
+    yy = json.loads(y)
+    dolar = pd.DataFrame(yy)
+    dolar['data'] = pd.to_datetime(dolar['data'],dayfirst=True)
+    dolar = dolar.sort_values('data')
+    dolar = dolar[dolar['data']>'01/01/2020']
+    dolar_plot = px.line(data_frame=dolar,x='data',y='valor')
+    return dolar_plot
 
-def plot_titulo(url):
-    titulo=pd.ExcelFile(url)
-    titulo_df =titulo.parse(0,header=[1])
-    fig = go.Figure()
-    for i in titulo_df.columns[1:]:
-        fig.add_trace(go.Scatter(x=titulo_df['Dia'],y=titulo_df[f'{i}'],name=f'{i}'))
-    return fig
+#def plot_titulo(url):
+#    titulo=pd.ExcelFile(url)
+#    titulo_df =titulo.parse(0,header=[1])
+#    fig = go.Figure()
+#    for i in titulo_df.columns[1:]:
+#        fig.add_trace(go.Scatter(x=titulo_df['Dia'],y=titulo_df[f'{i}'],name=f'{i}'))
+#    return fig
 
 def main():
-    dolar()
+    #dolar()
     dolar_plot = dolar()
     st.markdown("<h1 style='text-align: center; color: Black;'>Dolar</h1>", unsafe_allow_html=True)
     st.write(dolar_plot)
-    ltn = plot_titulo(LTN)
-    st.markdown("<h1 style='text-align: center; color: Black;'>LTN</h1>", unsafe_allow_html=True)
-    st.write(ltn)
+    ipca_plot - ipca()
+    st.markdown("<h1 style='text-align: center; color: Black;'>IPCA</h1>", unsafe_allow_html=True)
+    st.write(ipca_plot)
+
+
+    #ltn = plot_titulo(LTN)
+    #st.markdown("<h1 style='text-align: center; color: Black;'>LTN</h1>", unsafe_allow_html=True)
+    #st.write(ltn)
     #select = st.selectbox('Qual título você deseja visualizar?',['','LTN','LFT','NTNB Principal'])
     #if select == 'LTN':
     #    ltn = plot_titulo(LTN)
